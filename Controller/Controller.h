84c3b28bed0d95064eb0dc8e4ca2ed75610a4258
Kameshekk <43026756+Kameshekk@users.noreply.github.com>
2020-06-05 19:22:14 +0300

    Server know its IP

--- Controller/Controller.h
+++ Controller/Controller.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <WinSock2.h>
+#include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 #pragma warning(disable: 4996)
 
--- Controller/Server.cpp
+++ Controller/Server.cpp
@@ -10,10 +10,51 @@ Server::Server()
 		exit(1);
 	}
 	char IPadress[16];
-	cout << "Server's adress: "; 
-	cin.clear(); 
-	cin.ignore(INT_MAX, '\n');
-	cin.getline(IPadress, sizeof(IPadress));
+
+
+	const char* google_dns_server = "8.8.8.8";
+	int dns_port = 53;
+
+	struct sockaddr_in serv;
+	int sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0)
+	{
+		perror("Socket error");
+	}
+
+	memset(&serv, 0, sizeof(serv));
+	serv.sin_family = AF_INET;
+	serv.sin_addr.s_addr = inet_addr(google_dns_server);
+	serv.sin_port = htons(dns_port);
+
+	int err = connect(sock, (const struct sockaddr*) & serv, sizeof(serv));
+
+	struct sockaddr_in name;
+	socklen_t namelen = sizeof(name);
+	err = getsockname(sock, (struct sockaddr*) & name, &namelen);
+	const char* p = inet_ntop(AF_INET, &name.sin_addr, IPadress, 100);
+
+	if (p != NULL)
+	{
+		printf("Local ip is : %s \n", IPadress);
+	}
+	else
+	{
+		//Some error
+		printf("Error number : %d . Error message : %s \n", errno, strerror(errno));
+	}
+
+	//close(sock);
+
+
+
+
+
+
+	cout << "Server's adress: " << IPadress; 
+	//cin.clear(); 
+	//cin.ignore(INT_MAX, '\n');
+	//cin.getline(IPadress, sizeof(IPadress));
 	address.sin_addr.S_un.S_addr = inet_addr(IPadress);
 	address.sin_port = htons(9999);
 	address.sin_family = AF_INET;
@@ -39,6 +80,7 @@ bool Server::sended()
 {
 	char msg[256];
 	cin.clear();
+	cin.ignore(0, '\n');
 	cin.getline(msg, sizeof(msg));
 	send(Connection, msg, sizeof(msg), NULL);
 	if (strcmp(msg,"STOP"))
